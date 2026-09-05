26b79d81d753be2b828a521be0d2e381ff651bc9
AlexCao-html <caobubo@gmail.com>
2026-09-05 13:07:06 -0700

    finished the homepage

---
+++ script.js
@@ -0,0 +1,12 @@
+$(window).scroll(function() {
+    var scrollTop = $(window).scrollTop();
+    var scrollPercentage = scrollTop / $(window).height();
+    var scrollPercentage1 = ((scrollPercentage > 1) ? 1 : scrollPercentage);
+    var scrollPercentage2 = ((scrollPercentage < 1) ? 0 : (scrollPercentage - 1));
+    $("#problem").css("left", `calc(30vw - 5vmin - ${scrollPercentage1 * 100}vw)`);
+    $("#solution").css("left", `calc(130vw - 5vmin - ${scrollPercentage1 * 100}vw)`);
+    $("#solution").css("top", `calc(25vh - 5vmin - ${scrollPercentage2 * 100}vh)`);
+    $("#link").css("top", `calc(125vh - 5vmin - ${scrollPercentage2 * 100}vh)`);
+    $(".background1").css("opacity", `${1 - scrollPercentage1}`);
+    $(".background2").css("opacity", `${1 - scrollPercentage2}`);
+});
