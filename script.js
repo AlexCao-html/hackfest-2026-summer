18fcc54f336c90f2bb17656ecbf943fd45104a4e
AlexCao-html <caobubo@gmail.com>
2026-09-06 11:56:15 -0700

    finished flood senario and the separated one

--- script.js
+++ script.js
@@ -3,10 +3,14 @@ $(window).scroll(function() {
     var scrollPercentage = scrollTop / $(window).height();
     var scrollPercentage1 = ((scrollPercentage > 1) ? 1 : scrollPercentage);
     var scrollPercentage2 = ((scrollPercentage < 1) ? 0 : (scrollPercentage - 1));
+    var scrollPercentage2 = ((scrollPercentage > 2) ? 1 : (scrollPercentage2));
+    var scrollPercentage3 = ((scrollPercentage < 2) ? 0 : (scrollPercentage - 2));
     $("#problem").css("left", `calc(30vw - 5vmin - ${scrollPercentage1 * 100}vw)`);
     $("#solution").css("left", `calc(130vw - 5vmin - ${scrollPercentage1 * 100}vw)`);
     $("#solution").css("top", `calc(25vh - 5vmin - ${scrollPercentage2 * 100}vh)`);
-    $("#link").css("top", `calc(125vh - 5vmin - ${scrollPercentage2 * 100}vh)`);
+    $("#kit").css("top", `calc(125vh - 5vmin - ${scrollPercentage2 * 100}vh)`);
+    $("#kit").css("left", `calc(30vw - 5vmin + ${scrollPercentage3 * 100}vw)`);
+    $("#link").css("left", `calc(-70vw - 5vmin + ${scrollPercentage3 * 100}vw)`);
     $(".background1").css("opacity", `${1 - scrollPercentage1}`);
     $(".background2").css("opacity", `${1 - scrollPercentage2}`);
 });
--- simulation/script.js
+++ simulation/script.js
@@ -6,6 +6,7 @@ var emergency = false;
 var timer;
 var timesUp = false;
 var finished = false;
+var paused = false;
 $("#practice").click(() => {
     document.querySelector("#difficultyBtn").innerHTML = "Practice";
 });
@@ -55,6 +56,14 @@ $("#fireSenarioSelect").click(() => {
     $("#chooseSenarioConnected").addClass("inactive");
     $("#fireSenario").removeClass("inactive");
 });
+$("#floodSenarioSelect").click(() => {
+    $("#chooseSenarioConnected").addClass("inactive");
+    $("#floodSenario").removeClass("inactive");
+});
+$("#separated1SenarioSelect").click(() => {
+    $("#chooseSenarioSeparated").addClass("inactive");
+    $("#separatedSenario1").removeClass("inactive");
+});
 $(".start").click(() => {
     if (emergency) {
         $(".textbox").addClass("emergency");
@@ -62,15 +71,17 @@ $(".start").click(() => {
         timer = time;
         $(".timeLeft").html(timer);
         setInterval(() => {
-            timer--;
-            if (timer < 0) {
-                if (!finished) {
-                    points -= 2;
-                    timesUp = true;
-                    $(".pointsCount").html(points);
+            if (!paused) {
+                timer--;
+                if (timer < 0) {
+                    if (!finished) {
+                        points -= 2;
+                        timesUp = true;
+                        $(".pointsCount").html(points);
+                    }
+                } else {
+                    $(".timeLeft").html(timer);
                 }
-            } else {
-                $(".timeLeft").html(timer);
             }
         }, 1000);
         $(".answerChoice").click(() => {
@@ -79,145 +90,323 @@ $(".start").click(() => {
         });
     }
 });
+$("#separatedSenario1 .start").click(() => {
+    $("#separatedSenario1 .beginning").addClass("inactive");
+    $("#separatedSenario1 .q1").removeClass("inactive");
+});
 $("#fireSenario .start").click(() => {
     $("#fireSenario .beginning").addClass("inactive");
     $("#fireSenario .q1").removeClass("inactive");
 });
+$("#floodSenario .start").click(() => {
+    $("#floodSenario .beginning").addClass("inactive");
+    $("#floodSenario .q1").removeClass("inactive");
+});
+$(".answerChoice").click(() => {
+    timesUp = false;
+})
 $(".answerChoice.dangerous").click(() => {
-    points -= 5;
-    $(".pointsCount").html(points);
+    if (!paused) {
+        points -= 5;
+        $(".pointsCount").html(points);
+    }
 });
 $(".answerChoice.correct").click(() => {
-    if (!timesUp) {
-        points += 5;
-        $(".pointsCount").html(points);
+    if (!paused) {
+        if (!timesUp) {
+            points += 5;
+            $(".pointsCount").html(points);
+        }
     }
 });
 $("#fireSenario .q19 .answerChoice").click(() => {
-    $(".timeTicker").addClass("inactive");
-    finished = true;
+    if (!paused) {
+        $(".timeTicker").addClass("inactive");
+        finished = true;
+    }
 })
 $("#fireSenario .q20 .answerChoice").click(() => {
-    if (timesUp) {
-        points += 5;
-        $(".pointsCount").html(points);
+    if (!paused) {
+        if (timesUp) {
+            points += 5;
+            $(".pointsCount").html(points);
+        }
     }
 })
 $(".q1 .answerChoice").click(() => {
-    $(".q1").addClass("inactive")
-    $(".q2").removeClass("inactive")
-});
-$(".q2 .answerChoice").click(() => {
-    $(".q2").addClass("inactive")
-    $(".q3").removeClass("inactive")
-});
-$(".q3 .answerChoice").click(() => {
-    $(".q3").addClass("inactive")
-    $(".q4").removeClass("inactive")
-});
-$(".q4 .answerChoice").click(() => {
-    $(".q4").addClass("inactive")
-    $(".q5").removeClass("inactive")
-});
-$(".q5 .answerChoice").click(() => {
-    $(".q5").addClass("inactive")
-    $(".q6").removeClass("inactive")
-});
-$(".q6 .answerChoice").click(() => {
-    $(".q6").addClass("inactive")
-    $(".q7").removeClass("inactive")
-});
-$(".q7 .answerChoice").click(() => {
-    $(".q7").addClass("inactive")
-    $(".q8").removeClass("inactive")
-});
-$(".q8 .answerChoice").click(() => {
-    $(".q8").addClass("inactive")
-    $(".q9").removeClass("inactive")
-});
-$(".q9 .answerChoice").click(() => {
-    $(".q9").addClass("inactive")
-    $(".q10").removeClass("inactive")
-});
-$(".q10 .answerChoice").click(() => {
-    $(".q10").addClass("inactive")
-    $(".q11").removeClass("inactive")
-});
-$(".q11 .answerChoice").click(() => {
-    $(".q11").addClass("inactive")
-    $(".q12").removeClass("inactive")
-});
-$(".q12 .answerChoice").click(() => {
-    $(".q12").addClass("inactive")
-    $(".q13").removeClass("inactive")
-});
-$(".q13 .answerChoice").click(() => {
-    $(".q13").addClass("inactive")
-    $(".q14").removeClass("inactive")
-});
-$(".q14 .answerChoice").click(() => {
-    $(".q14").addClass("inactive")
-    $(".q15").removeClass("inactive")
-});
-$(".q15 .answerChoice").click(() => {
-    $(".q15").addClass("inactive")
-    $(".q16").removeClass("inactive")
-});
-$(".q16 .answerChoice").click(() => {
-    $(".q16").addClass("inactive")
-    $(".q17").removeClass("inactive")
-});
-$(".q17 .answerChoice").click(() => {
-    $(".q17").addClass("inactive")
-    $(".q18").removeClass("inactive")
-});
-$(".q18 .answerChoice").click(() => {
-    $(".q18").addClass("inactive")
-    $(".q19").removeClass("inactive")
-});
-$(".q19 .answerChoice").click(() => {
-    $(".q19").addClass("inactive")
-    $(".q20").removeClass("inactive")
-});
-$(".q20 .answerChoice").click(() => {
-    $(".q20").addClass("inactive")
-    $(".results").removeClass("inactive")
-    if (points === 100) {
-        $(".rank").html("SSS")
-        $(".rankTitle").html("Safety Grandmaster")
-        $(".rankDesc").html("The highest level of emergency-response proficiency and decision-making.")
-    } else if (points >= 95) {
-        $(".rank").html("SS")
-        $(".rankTitle").html("Incident Commander")
-        $(".rankDesc").html("Demonstrates outstanding leadership, coordination, and crisis management.")
-    } else if (points >= 90) {
-        $(".rank").html("S")
-        $(".rankTitle").html("Emergency Specialist")
-        $(".rankDesc").html("Exceptionally skilled in complex and high-pressure situations.")
-    } else if (points >= 80) {
-        $(".rank").html("A")
-        $(".rankTitle").html("Senior Responder")
-        $(".rankDesc").html("Highly capable of managing serious incidents and assisting others.")
-    } else if (points >= 70) {
-        $(".rank").html("B")
-        $(".rankTitle").html("Qualified Responder")
-        $(".rankDesc").html("Demonstrates solid emergency-response skills and sound judgment.")
-    } else if (points >= 60) {
-        $(".rank").html("C")
-        $(".rankTitle").html("Junior Responder")
-        $(".rankDesc").html("Can respond to routine incidents and follow established protocols.")
-    } else if (points >= 40) {
-        $(".rank").html("D")
-        $(".rankTitle").html("Novice Responder")
-        $(".rankDesc").html("Understands basic emergency procedures but has limited experience.")
-    } else if (points >= -10) {
-        $(".rank").html("E")
-        $(".rankTitle").html("Untrained Civilian")
-        $(".rankDesc").html("Has minimal knowledge of emergency response and safety protocols.")
-    } else {
-        $(".rank").html("F")
-        $(".rankTitle").html("Unaware Civilian")
-        $(".rankDesc").html("Has little to no awareness of emergency procedures or public safety.")
-
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q1").addClass("inactive")
+            $(".q2").removeClass("inactive")
+        }, 3000)
     }
 });
+$(".q2 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q2").addClass("inactive")
+            $(".q3").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q3 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q3").addClass("inactive")
+            $(".q4").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q4 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q4").addClass("inactive")
+            $(".q5").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q5 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q5").addClass("inactive")
+            $(".q6").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q6 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q6").addClass("inactive")
+            $(".q7").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q7 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q7").addClass("inactive")
+            $(".q8").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q8 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q8").addClass("inactive")
+            $(".q9").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q9 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q9").addClass("inactive")
+            $(".q10").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q10 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q10").addClass("inactive")
+            $(".q11").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q11 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q11").addClass("inactive")
+            $(".q12").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q12 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q12").addClass("inactive")
+            $(".q13").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q13 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q13").addClass("inactive")
+            $(".q14").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q14 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q14").addClass("inactive")
+            $(".q15").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q15 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q15").addClass("inactive")
+            $(".q16").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q16 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q16").addClass("inactive")
+            $(".q17").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q17 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q17").addClass("inactive")
+            $(".q18").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q18 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q18").addClass("inactive")
+            $(".q19").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q19 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q19").addClass("inactive")
+            $(".q20").removeClass("inactive")
+        }, 3000)
+    }
+});
+$(".q20 .answerChoice").click(() => {
+    if (!paused) {
+        paused = true;
+        $(".answerChoice").addClass("showColor");
+        setTimeout(() => {
+            $(".answerChoice").removeClass("showColor");
+            paused = false;
+            $(".q20").addClass("inactive")
+            $(".results").removeClass("inactive")
+            if (points === 100) {
+                $(".rank").html("SSS")
+                $(".rankTitle").html("Master Responder")
+                $(".rankDesc").html("The highest level of emergency-response proficiency and decision-making.")
+            } else if (points >= 95) {
+                $(".rank").html("SS")
+                $(".rankTitle").html("Incident Commander")
+                $(".rankDesc").html("Demonstrates outstanding leadership, coordination, and crisis management.")
+            } else if (points >= 90) {
+                $(".rank").html("S")
+                $(".rankTitle").html("Emergency Specialist")
+                $(".rankDesc").html("Exceptionally skilled in complex and high-pressure situations.")
+            } else if (points >= 80) {
+                $(".rank").html("A")
+                $(".rankTitle").html("Senior Responder")
+                $(".rankDesc").html("Highly capable of managing serious incidents and assisting others.")
+            } else if (points >= 70) {
+                $(".rank").html("B")
+                $(".rankTitle").html("Qualified Responder")
+                $(".rankDesc").html("Demonstrates solid emergency-response skills and sound judgment.")
+            } else if (points >= 60) {
+                $(".rank").html("C")
+                $(".rankTitle").html("Junior Responder")
+                $(".rankDesc").html("Can respond to routine incidents and follow established protocols.")
+            } else if (points >= 40) {
+                $(".rank").html("D")
+                $(".rankTitle").html("Novice Responder")
+                $(".rankDesc").html("Understands basic emergency procedures but has limited experience.")
+            } else if (points >= -10) {
+                $(".rank").html("E")
+                $(".rankTitle").html("Untrained Civilian")
+                $(".rankDesc").html("Has minimal knowledge of emergency response and safety protocols.")
+            } else {
+                $(".rank").html("F")
+                $(".rankTitle").html("Unaware Civilian")
+                $(".rankDesc").html("Has little to no awareness of emergency procedures or public safety.")
+            }
+        }, 3000)
+    }
+})
